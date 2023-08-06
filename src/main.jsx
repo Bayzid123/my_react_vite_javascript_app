98524a14c1dd2b851a883ec97740dbb874a7ad2b
Bayzid123 <bayzid18103384@gmail.com>
2023-08-06 12:57:22 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+setInterval(() => {
+  const element = <h1>Hello World {new Date().toLocaleTimeString()}</h1>;
+  ReactDOM.render(element, document.getElementById("root"));
+}, 1000);
